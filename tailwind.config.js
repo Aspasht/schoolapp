c47ad108de166105a795f98f4b452847afa25c0f
sagar pariyar <72987297+Aspasht@users.noreply.github.com>
2025-01-01 06:45:46 +0300

    modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import * as tailwindAnimate from "tailwindcss-animate"
 export default {
     darkMode: ["class"],
     content: [
@@ -56,5 +57,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: ["tailwindcss-animate"],
  }
